23220f26e89385fae1e61889e2fcd70ee00095d5
James Pier <jpier2012@gmail.com>
2019-05-21 16:52:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouse < ActiveRecord::Migration[5.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
     end
   end
 end
