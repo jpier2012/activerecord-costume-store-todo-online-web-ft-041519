b3801d96164524ca4abb2e8809e671efabcb7201
James Pier <jpier2012@gmail.com>
2019-05-21 16:51:12 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,8 +5,8 @@ class CreateHauntedHouse < ActiveRecord::Migration[5.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
+      t.string :theme
+      t.integer :price
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
